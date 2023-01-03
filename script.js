90bd7f5197c715fa5f7b7657caafab17ba08b6ea
hp <sahalnurdin@gmail.com>
2023-01-04 06:29:27 +0700

    add changes

--- script.js
+++ script.js
@@ -13,28 +13,28 @@ $("#audio-control").click(function () {
   }
 });
 
-$("#audio-control").click(function () {
-  if ($("#video-bg").prop("muted")) {
-    $("#video-bg").prop("muted", false);
-    // $(this).text("Mute");
-    // or toggle class, style it with a volume icon sprite, change background-position
-    // $(this).html("<span><i class='fas fa-volume-up'></i></span>");
-  } else {
-    $("#video-bg").prop("muted", true);
-    // $(this).text("Unmute");
-    // $(this).html("<span><i class='fas fa-volume-mute'></i></span>");
-  }
-});
+// $("#audio-control").click(function () {
+//   if ($("#video-bg").prop("muted")) {
+//     $("#video-bg").prop("muted", false);
+//     // $(this).text("Mute");
+//     // or toggle class, style it with a volume icon sprite, change background-position
+//     // $(this).html("<span><i class='fas fa-volume-up'></i></span>");
+//   } else {
+//     $("#video-bg").prop("muted", true);
+//     // $(this).text("Unmute");
+//     // $(this).html("<span><i class='fas fa-volume-mute'></i></span>");
+//   }
+// });
 
-$("#audio-control").click(function () {
-  if ($("#video-bg").prop("muted")) {
-    $("#video-bg").prop("muted", false);
-    // $(this).text("Mute");
-    // or toggle class, style it with a volume icon sprite, change background-position
-    // $(this).html("<span><i class='fas fa-volume-up'></i></span>");
-  } else {
-    $("#video-bg").prop("muted", true);
-    // $(this).text("Unmute");
-    // $(this).html("<span><i class='fas fa-volume-mute'></i></span>");
-  }
-});
+// $("#audio-control").click(function () {
+//   if ($("#video-bg").prop("muted")) {
+//     $("#video-bg").prop("muted", false);
+//     // $(this).text("Mute");
+//     // or toggle class, style it with a volume icon sprite, change background-position
+//     // $(this).html("<span><i class='fas fa-volume-up'></i></span>");
+//   } else {
+//     $("#video-bg").prop("muted", true);
+//     // $(this).text("Unmute");
+//     // $(this).html("<span><i class='fas fa-volume-mute'></i></span>");
+//   }
+// });
