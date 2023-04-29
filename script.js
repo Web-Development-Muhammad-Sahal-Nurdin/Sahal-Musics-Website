6fefedd0dbd8e1371088ce896654122cb0ae44e6
Muhammad Sahal Nurdin <sahalnurdin@gmail.com>
2023-04-29 13:02:17 +0700

    add changes

--- script.js
+++ script.js
@@ -3,12 +3,7 @@
 $("#audio-control").click(function () {
   if ($("#video-bg").prop("muted")) {
     $("#video-bg").prop("muted", false);
-    // $(this).text("Mute");
-    // or toggle class, style it with a volume icon sprite, change background-position
-    // $(this).html("<span><i class='fas fa-volume-up'></i></span>");
   } else {
     $("#video-bg").prop("muted", true);
-    // $(this).text("Unmute");
-    // $(this).html("<span><i class='fas fa-volume-mute'></i></span>");
   }
 });
